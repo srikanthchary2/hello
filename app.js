3622632ebc3978e41ae47a227177cacb175e76e8
geetha1234567890 <60750875+geetha1234567890@users.noreply.github.com>
2024-07-09 19:53:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, how are u? Hi geetha \n');
-  res.end('Hello, how are u? Hi geetha \n');
+  res.end('Hello, how are u? Hi geetha, how are u \n');
 });
 
 server.listen(port, hostname, () => {
